4b91d1215ba6816cc9d7e72ec5171f0c89afa0db
Victor Tavares <victor.as156@gmail.com>
2021-04-16 14:53:03 -0300

    Utilizando somente JsonIgnore

--- src/main/java/com/victor/springprojectbackend/domain/Cidade.java
+++ src/main/java/com/victor/springprojectbackend/domain/Cidade.java
@@ -10,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 
 @Entity
 public class Cidade implements Serializable {
@@ -23,7 +23,6 @@ public class Cidade implements Serializable {
 	private Integer id;
 	private String nome;
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="estado_id")
 	private Estado estado;
