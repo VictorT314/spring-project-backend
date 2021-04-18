b3ccde3f2e369230817b6270034740ddaeb1b219
Victor Tavares <victor.as156@gmail.com>
2021-04-17 22:56:12 -0300

    Configurando DELETE para Categoria

--- src/main/java/com/victor/springprojectbackend/services/CategoriaService.java
+++ src/main/java/com/victor/springprojectbackend/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.victor.springprojectbackend.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.victor.springprojectbackend.domain.Categoria;
 import com.victor.springprojectbackend.repositories.CategoriaRepository;
+import com.victor.springprojectbackend.services.exceptions.DataIntegrityException;
 import com.victor.springprojectbackend.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+		}
 	}
 }
