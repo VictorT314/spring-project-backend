a4a3c2b867f5544c4cadeabcdfd54b374c80e623
Victor Tavares <victor.as156@gmail.com>
2021-04-19 11:28:59 -0300

    Configurando paginação com parâmetros opcionais na requisição

--- src/main/java/com/victor/springprojectbackend/services/CategoriaService.java
+++ src/main/java/com/victor/springprojectbackend/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.victor.springprojectbackend.domain.Categoria;
@@ -14,14 +17,8 @@ import com.victor.springprojectbackend.services.exceptions.ObjectNotFoundExcepti
 
 @Service
 public class CategoriaService {
-
 	@Autowired
 	private CategoriaRepository repo;
-	
-	public List<Categoria> findAll(){
-		return repo.findAll();
-	}
-	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
@@ -44,7 +41,16 @@ public class CategoriaService {
 			repo.deleteById(id);
 		}
 		catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
